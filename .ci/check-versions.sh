f8e0a6b2a9a24e52e84a40552cbdb4d48e48f1db
Kevin Earls <kearls@redhat.com>
2020-04-10 17:26:44 +0200

    Add version checks
    Signed-off-by: Kevin Earls <kearls@redhat.com>

---
+++ .ci/check-versions.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit immediately when a command fails
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+# error on unset variables
+set -u
+# print each command before executing it
+set -x
+
+minikube version
+kubectl version
+
+kubectl get all --namespace default
+kubectl get namespaces
