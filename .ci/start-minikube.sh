d288bbaa4bccf431d5a7102f2a9695eebdb0f6d0
Kevin Earls <kearls@redhat.com>
2020-04-14 13:51:08 +0200

    Throwing stuff at the wall
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- .ci/start-minikube.sh
+++ .ci/start-minikube.sh
@@ -41,8 +41,17 @@ touch "${HOME}"/.kube/config
 minikube config set WantNoneDriverWarning false
 minikube config set vm-driver none
 
+
+cat /proc/sys/net/bridge/bridge-nf-call-iptables
+sleep 2
+
+
 minikube version
 sudo ${MINIKUBE} delete
+echo "1" >/proc/sys/net/bridge/bridge-nf-call-iptables
+cat /proc/sys/net/bridge/bridge-nf-call-iptables
+sleep 2
+
 sudo ${MINIKUBE} start --kubernetes-version=$KUBERNETES_VERSION --extra-config=apiserver.authorization-mode=RBAC
 sudo chown -R $USER $HOME/.kube $HOME/.minikube
 
