9f3906443f4e32c89e9039bcc59790840d7c8da6
Kevin Earls <kearls@redhat.com>
2020-04-14 10:04:44 +0200

    Use minikube 1.6.2; try dns wait
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- .ci/start-minikube.sh
+++ .ci/start-minikube.sh
@@ -50,7 +50,7 @@ JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.ty
 # kubectl -n kube-system get pods -lk8s-app=kube-dns
 kubectl get pods --all-namespaces
 
-#JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl -n kube-system get pods -lk8s-app=kube-dns -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1;echo "waiting for kube-dns to be available"; kubectl get pods --all-namespaces; done
+JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl -n kube-system get pods -lk8s-app=kube-dns -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1;echo "waiting for kube-dns to be available"; kubectl get pods --all-namespaces; done
 
 sudo ${MINIKUBE} addons enable ingress
 
