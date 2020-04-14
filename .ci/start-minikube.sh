fb5cf4295aaf83420c2ee3d4ede62898171796ff
Kevin Earls <kearls@redhat.com>
2020-04-14 13:54:32 +0200

    Add sudo to fix command
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- .ci/start-minikube.sh
+++ .ci/start-minikube.sh
@@ -48,7 +48,7 @@ sleep 2
 
 minikube version
 sudo ${MINIKUBE} delete
-echo "1" >/proc/sys/net/bridge/bridge-nf-call-iptables
+sudo echo "1" >/proc/sys/net/bridge/bridge-nf-call-iptables
 cat /proc/sys/net/bridge/bridge-nf-call-iptables
 sleep 2
 
